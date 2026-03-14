663815c6d43c9179a462f003d32379e08c19bb08
Om Gutty <om.gutty@gmail.com>
2026-03-14 11:08:03 +0530

    enum and project structure

---
+++ src/FirstTSProgram.ts
@@ -0,0 +1,32 @@
+
+
+let a:number= 5;
+
+console.log(a);
+
+
+let b:string="om";
+console.log(b);
+
+let c:number=25;
+console.log(c);
+
+function add (a:number, b:number):number{
+    console.log(a+b);
+    return a+b;
+}
+
+console.log(add(3,4));
+
+let x:any;
+//any means any data type 
+
+x= 'om';
+x=12;
+x=12.12;
+
+
+console.log(x);
+
+
+
---
+++ src/ennums.ts
@@ -0,0 +1,15 @@
+//enumm  is used to to collection fo constants. 
+
+enum Browser{
+
+    chrome= 'Google chrome',
+    firefox='mozilla',
+}
+
+console.log(Browser.chrome);
+enum env{
+    QA='www.qa.com',
+    prod='www.production.com'
+}
+
+console.log(env.prod);
